86a1a536869076fc544a9613a3ceb5eefda32886
Hyun Bin Yim <hby0818@gmail.com>
2023-02-13 15:05:55 -0500

    morestate

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 import Header from './components/Header'
-import Home from './pages/Home'
+import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Main from './pages/Main'
 // import Project from './pages/Project'
 
 function App() {
+  const [currentState, setCurrentState] = useState('login')
+  
   return (
     <div className='appContainer'>
       <Header />
       <div className='routeContainer'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/main" element={<Main />} />
-        </Routes>
+        {
+          currentState === 'login' ? <Login /> : <Signup/>
+        }
+
       </div>
     </div>
   );
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,55 @@
-const Signup = () => {
+import { useState } from 'react'
+
+export const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email)
+    }
+
     return (
-        <div>Signup</div>
+        <div className="homeContainer">
+        <form className="loginBox" onSubmit={handleSubmit}>
+            <h1>Log In</h1>
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+                placeholder='youremail@gmail.com'
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e)=> setEmail(e.target.value)}
+            />
+            <br />
+            <label htmlFor="name">Full Name</label>
+            <br />
+            <input
+                placeholder='Full Name'
+                id="name"
+                type="name"
+                value={name}
+                onChange={(e)=> setName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+                placeholder='*******'
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e)=> setPassword(e.target.value)}
+            />
+            <br />
+            <button type="submit">Log In</button>
+            <br />
+            <button>Signup</button>
+        </form>
+    </div>
     )
 }
 export default Signup
