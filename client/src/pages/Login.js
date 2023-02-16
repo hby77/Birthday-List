9839b75e228bb59d0cb7a175ad8090a32ed78eb9
Hyun Bin Yim <hby0818@gmail.com>
2023-02-16 17:27:39 -0500

    cssLogin

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -37,39 +37,38 @@ const Home = (props) => {
 
 
     return (
-        <div className='loginbackground'>
-            <div className="homeContainer">
-                <form className="loginBox" onSubmit={handleSubmit}>
-                    <h1 className='loginname'>Welcome Back</h1>
-                    <h4 className='signtext'>Sign into your account</h4>
-                    <label htmlFor="email"></label>
-                    <br />
-                    <input
-                        className='textboxlogin'
-                        placeholder='Email'
-                        id="email"
-                        type="email"
-                        value={formStates.email}
-                        onChange={(e) => setFormStates({ ...formStates, [e.target.id]: e.target.value })}
-                    />
-                    <br />
-                    <label htmlFor="password"></label>
-                    <br />
-                    <input
-                        className='textboxlogin'
-                        placeholder='Password'
-                        id="password"
-                        type="password"
-                        value={formStates.password}
-                        onChange={(e) => setFormStates({ ...formStates, [e.target.id]: e.target.value })}
-                    />
-                    <br />
-                    <button className='loginbutton' type="submit">Log In</button>
-                    <br />
-                    <button className='loginbutton' onClick={() => props.onFormSwitch('signup')}>Don't have an account? Signup Here.</button>
-                </form>
+
+            <div className='loginbackground'>
+                <div className="homeContainer">
+                    <form className="loginBox" onSubmit={handleSubmit}>
+                        <h1 className='loginname'>Welcome Back</h1>
+                        <h4 className='signtext'>Sign into your account</h4>
+                        <label htmlFor="email"></label>
+                        <input
+                            className='textboxlogin'
+                            placeholder='Email'
+                            id="email"
+                            type="email"
+                            value={formStates.email}
+                            onChange={(e) => setFormStates({ ...formStates, [e.target.id]: e.target.value })}
+                        />
+                        <br />
+                        <label htmlFor="password"></label>
+                        <input
+                            className='textboxlogin'
+                            placeholder='Password'
+                            id="password"
+                            type="password"
+                            value={formStates.password}
+                            onChange={(e) => setFormStates({ ...formStates, [e.target.id]: e.target.value })}
+                        />
+                        <br />
+                        <button className='submitbutton' type="submit">Sign In</button>
+                        <br />
+                        <label><a href='#' className='loginlink' onClick={() => props.onFormSwitch('signup')}>Don't have an account? Signup Here.</a></label> 
+                    </form>
+                </div>
             </div>
-        </div>
     )
 }
 export default Home
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -16,51 +16,52 @@ const Signup = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         const res = await axios.post('/api/createUser', formState)
-        
+
         setFormState(initialState)
     }
 
     return (
-        <div className="homeContainer">
-            <form className="loginBox" onSubmit={handleSubmit}>
-                <h1>Sign Up</h1>
-                <label htmlFor="email"></label>
-                <br />
-                <input
-                    className='textboxlogin'
-                    placeholder='Email'
-                    id="email"
-                    type="email"
-                    value={formState.email}
-                    onChange={handleChange}
-                />
-                <br />
-                <label htmlFor="name"></label>
-                <br />
-                <input
-                    className='textboxlogin'
-                    placeholder='Name'
-                    id="name"
-                    type="name"
-                    value={formState.name}
-                    onChange={handleChange}
-                />
-                <br />
-                <label htmlFor="password"></label>
-                <br />
-                <input
-                    className='textboxlogin'
-                    placeholder='Password'
-                    id="password"
-                    type="password"
-                    value={formState.password}
-                    onChange={handleChange}
-                />
-                <br />
-                <button className='loginbutton' type="submit">Sign Up</button>
-                <br />
-                <button className='loginbutton' onClick={() => props.onFormSwitch('login')}>Already have an account? Login Here.</button>
-            </form>
+        <div className='loginbackground'>
+            <div className="homeContainer">
+                <form className="loginBox" onSubmit={handleSubmit}>
+                    <h1 className='loginname'>Sign Up</h1>
+                    <label htmlFor="email"></label>
+                    <br />
+                    <br />
+                    <input
+                        className='textboxlogin'
+                        placeholder='Email'
+                        id="email"
+                        type="email"
+                        value={formState.email}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="name"></label>
+                    <br />
+                    <input
+                        className='textboxlogin'
+                        placeholder='Name'
+                        id="name"
+                        type="name"
+                        value={formState.name}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="password"></label>
+                    <br />
+                    <input
+                        className='textboxlogin'
+                        placeholder='Password'
+                        id="password"
+                        type="password"
+                        value={formState.password}
+                        onChange={handleChange}
+                    />
+                    <br />
+                    <button className='submitbutton' type="submit">Sign Up</button>
+                    <br />
+                    <label><a href='#' className='loginlink' onClick={() => props.onFormSwitch('login')}>Already have an account? Login Here.</a></label> 
+                </form>
+            </div>
         </div>
     )
 }
