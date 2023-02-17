8a76c06ac8171f6cb590d54fcdd86ed09686eb8a
Hyun Bin Yim <hby0818@gmail.com>
2023-02-17 01:58:58 -0500

    css3

--- client/src/pages/NewProject.js
+++ client/src/pages/NewProject.js
@@ -46,7 +46,7 @@ const NewProject = () => {
             formState.birthday !== '' &&
             formState.companyAndCareer !== '' &&
             formState.hobbiesAndExpertise !== '' &&
-            formState.notes !== '' && 
+            formState.notes !== '' &&
             titleState.title !== ''
         ) {
             const postData = await axios.post("/api/createData", formState)
@@ -60,10 +60,10 @@ const NewProject = () => {
     }
 
 
-const handleDelete = async(id) => {
-    await axios.delete(`/api/deleteDatas/${id}`)
-    getData()
-}
+    const handleDelete = async (id) => {
+        await axios.delete(`/api/deleteDatas/${id}`)
+        getData()
+    }
 
 
 
@@ -71,94 +71,98 @@ const handleDelete = async(id) => {
         getData()
     }, [])
 
-console.log(project)
+    console.log(project)
 
     return data && (
-        <div className='databackground'>
-            <div>
-                <h1>{project.title}</h1>
-                <Header />
-                <form onSubmit={handleSubmit}>
-                    <input
-                        name='title'
-                        type='text'
-                        placeholder='project title'
-                        value={titleState.relationship}
-                        onChange={(e) => setTitleState({ ...titleState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='relationship'
-                        type='text'
-                        placeholder='relationship'
-                        value={formState.relationship}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='person'
-                        type='text'
-                        placeholder='person'
-                        value={formState.person}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='currentLocation'
-                        type='text'
-                        placeholder='currentLocation'
-                        value={formState.currentLocation}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='association'
-                        type='text'
-                        placeholder='association'
-                        value={formState.association}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='birthday'
-                        type='text'
-                        placeholder='birthday'
-                        value={formState.birthday}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='companyAndCareer'
-                        type='text'
-                        placeholder='companyAndCareer'
-                        value={formState.companyAndCareer}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='hobbiesAndExpertise'
-                        type='text'
-                        placeholder='hobbiesAndExpertise'
-                        value={formState.hobbiesAndExpertise}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <input
-                        name='notes'
-                        type='text'
-                        placeholder='notes'
-                        value={formState.notes}
-                        onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-                    />
-                    <button type="submit">+ Add</button>
-                </form>
-            </div>
-            <div>
-                {data.map((item) => (
-                    <div key={item._id} className='rowdata'>
-                        <p>{item.relationship}</p>
-                        <p>{item.person}</p>
-                        <p>{item.currentLocation}</p>
-                        <p>{item.association}</p>
-                        <p>{item.birthday}</p>
-                        <p>{item.companyAndCareer}</p>
-                        <p>{item.hobbiesAndExpertise}</p>
-                        <p>{item.notes}</p>
-                    <button onClick={() => handleDelete(item._id)}>Delete</button>
+        <div className='backgroundcontainer'>
+            <div className='databackground'>
+                <div>
+                    <Header />
+                    <div className='projecttitlecontainer'>
+                        <h1 className='projecttitle'>{project.title}</h1>
                     </div>
-                ))}
+                    <form className='formthing' onSubmit={handleSubmit}>
+                        <input
+                            name='title'
+                            type='text'
+                            placeholder='project title'
+                            value={titleState.relationship}
+                            onChange={(e) => setTitleState({ ...titleState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='relationship'
+                            type='text'
+                            placeholder='relationship'
+                            value={formState.relationship}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='person'
+                            type='text'
+                            placeholder='person'
+                            value={formState.person}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='currentLocation'
+                            type='text'
+                            placeholder='currentLocation'
+                            value={formState.currentLocation}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='association'
+                            type='text'
+                            placeholder='association'
+                            value={formState.association}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='birthday'
+                            type='text'
+                            placeholder='birthday'
+                            value={formState.birthday}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='companyAndCareer'
+                            type='text'
+                            placeholder='companyAndCareer'
+                            value={formState.companyAndCareer}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='hobbiesAndExpertise'
+                            type='text'
+                            placeholder='hobbiesAndExpertise'
+                            value={formState.hobbiesAndExpertise}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <input
+                            name='notes'
+                            type='text'
+                            placeholder='notes'
+                            value={formState.notes}
+                            onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        />
+                        <button className='addbutton' type="submit">+ Add</button>
+                    </form>
+                </div>
+                <div>
+                    {data.map((item) => (
+                        <div key={item._id} className='rowdata'>
+                            <p>{item.relationship}</p>
+                            <p>{item.person}</p>
+                            <p>{item.currentLocation}</p>
+                            <p>{item.association}</p>
+                            <p>{item.birthday}</p>
+                            <p>{item.companyAndCareer}</p>
+                            <p>{item.hobbiesAndExpertise}</p>
+                            <p>{item.notes}</p>
+                            <button className='deletebutton' onClick={() => handleDelete(item._id)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
